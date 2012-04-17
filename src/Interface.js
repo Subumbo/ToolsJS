3d8f142400f3aa5c4e6cb56df931b05e89209b11
Patrick Wolleb <pat.wolleb@gmail.com>
2012-04-17 09:50:54 +0100

    interface example added

--- src/Interface.js
+++ src/Interface.js
@@ -17,7 +17,6 @@
  * 
  */
 var Interface = function(name, methods) {
-	
 	if(arguments.length != 2)  throw new Error('Interface::constructor -- Incorrect number of arguments: Expected 2 received ' + arguments.length);
 	
 	this.name = name;
@@ -34,13 +33,14 @@ Interface.check = function(object) {
 	if(arguments.length < 2) throw new Error('Interface::check -- Incorrect number of arguments: Expected at least 2 received ' + arguments.length);
 	
 	var interFace, m;
+	
 	for(var i=1, l = arguments.length; i < l; ++i) {
 		interFace = arguments[i];
 		if(interFace.constructor != Interface) throw new Error('Interface::check -- Arguments 2 and above expected to be of type Interface');
 		
 		for(var j=0, l=interFace.methods.length; j < l; ++j) {
-			m = interFace.methods[i];
-			if(!object[m] || typeof object[m] !== 'function')  throw new Error('Function Interface.ensureImplements: object does not implement ' + interFace.name + 'Method ' + method + ' was not found.');
+			m = interFace.methods[j];
+			if(!object[m] || typeof object[m] !== 'function')  throw new Error('Interface::check: object does not implement ' + interFace.name + 'Method ' + m + ' was not found.');
 		}
 	}
 }
